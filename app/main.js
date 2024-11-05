346abccd36ced2f83b0703c3933f3a43283c41d8
Paul M Kallarackal <paul.m@surveysparrow.com>
2024-11-05 10:39:34 +0530

    multiple_connections_event_loop

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ const net = require("net");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handles multiple connections due to event loop in Javascript
+  // Already has an inbuilt event loop for concurrent connections
     connection.on("data", (data) => {
         connection.write("+PONG\r\n")
     });
