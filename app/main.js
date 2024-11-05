52acc2a96a71222c029ddd6c468c79a2418e009b
Paul M Kallarackal <paul.m@surveysparrow.com>
2024-11-05 10:35:36 +0530

    multiple_connections

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ const net = require("net");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  // Handles multiple connections due to event loop in Javascript
     connection.on("data", (data) => {
         connection.write("+PONG\r\n")
     });
