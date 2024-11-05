2e7fb446dbde2e286d15629b7f4f26ce2d3df7a8
Paul M Kallarackal <paul.m@surveysparrow.com>
2024-11-05 11:15:44 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,22 +1,26 @@
 const net = require("net");
 
-
-// Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handles multiple connections due to event loop in Javascript
   // Already has an inbuilt event loop for concurrent connections
   // handling ECHO as RESP Bulk String both input and output format, case insensitive -> $<length>\r\n<data>\r\n
-  
+  const Store = {}
   //Need to refactor the arguments
     connection.on("data", (data) => {
         const DataString = Decoder(data.toString())
         if(DataString[2].toUpperCase()==='PING'){
-          connection.write("+PONG\r\n")
+          connection.write("+PONG\r\n");
         }
         if(DataString[2].toUpperCase()==='ECHO'){
-            connection.write(Encoder(DataString[4]))
+            connection.write(Encoder(DataString[4]));
         }
-     
+        if(DataString[2].toUpperCase()==='SET'){
+            Store[DataString[4]] = DataString[6];
+            connection.write(Encoder('OK'))
+        }
+        if(DataString[2].toUpperCase()==='GET'){
+            connection.write(Encoder(Store[DataString[4]]));
+        }
     });
 });
 
@@ -27,5 +31,7 @@ const Encoder = (data) => {
 const Decoder = (data) => {
     return  data.split('\r\n')
 }
+
+
 
 server.listen(6379, "127.0.0.1");
