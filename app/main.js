426075395492466e49529eae429c2eef02ae92d4
Paul M Kallarackal <paul.m@surveysparrow.com>
2024-11-05 17:36:23 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,44 +1,56 @@
-const { connect } = require("http2");
 const net = require("net");
 
 const server = net.createServer((connection) => {
-    process.argv.forEach(function (val, index, array) {
-        console.log(index + ': ' + val);
-    });
-  const Store = {}
+  const Store = new Map()
+  const arguments = process.argv;
+
   //Need to refactor the arguments
     connection.on("data", (data) => {
-        const DataString = Decoder(data.toString())
-        if(DataString[2].toUpperCase()==='PING'){
-          connection.write("+PONG\r\n");
-        }
-        if(DataString[2].toUpperCase()==='ECHO'){
-            connection.write(Encoder(DataString[4]));
-        }
-        if(DataString[2].toUpperCase()==='SET'){
-            if(DataString[8]?.toUpperCase()==='PX'){
-                Store[DataString[4]] = [DataString[6],Date.now(),DataString[10]];
-            } else {
-                Store[DataString[4]] = [DataString[6],"",""];
-            }          
-            connection.write(Encoder('OK'))
-        }
-        if(DataString[2].toUpperCase()==='GET'){
-            if(Store[DataString[4]][1]){
-                const timeDifference = (Date.now() - Store[DataString[4]][1])
-                const setTime = Store[DataString[4]][2];
-                const condition = timeDifference > setTime;
-                const data =  condition?  null: Store[DataString[4]][0];
-                data ? connection.write(Encoder(data)) : connection.write("$-1\r\n");
-            } else {
-                connection.write(Encoder(Store[DataString[4]][0]))
-            }
+        const commandList = Decoder(data.toString().toLowerCase());
+        const command = commandList[2];
+        switch(command){
+            case 'ping': connection.write("+PONG\r\n"); break;
+            case 'echo': connection.write(Encoder(commandList[4])); break;
+            case 'set': 
+                Store[commandList[4]] = [commandList[6],Date.now(),commandList[10]];
+                connection.write(Encoder('OK'));
+                break;
+            case 'get':
+                if(Store[commandList[4]][1]){
+                    const timeDifference = (Date.now() - Store[commandList[4]][1])
+                    const setTime = Store[commandList[4]][2];
+                    const condition = timeDifference > setTime;
+                    const data =  condition?  null: Store[commandList[4]][0];
+                    connection.write(Encoder(data));
+                    break;
+                } else {
+                    connection.write(Encoder(Store[commandList[4]][0]))
+                    break;
+                }
+            case 'config':
+                if(commandList[6]==='dir'){
+                    connection.write(Encoder(['dir',process.argv[3]]));;   
+                };
+                if(commandList[6]==='dbfilename'){
+                    connection.write(Encoder(['dbfilename',process.argv[5]]));;  
+                }
         }
     });
 });
 
 const Encoder = (data) => {
-    return `$${data.length}\r\n${data}\r\n`;
+    const dataType = Array.isArray(data) ? 'list' : typeof data ;
+    switch(dataType){
+        case 'string': return `$${data.length}\r\n${data}\r\n`; 
+        case 'list': 
+            let dataValues = '';
+            data.forEach(element => {
+                dataValues += `$${element.length}\r\n${element}\r\n`;
+            });
+            return `*${data.length}\r\n${dataValues}`;
+        default:
+            return `$-1\r\n`;
+    }
 }
 
 const Decoder = (data) => {
